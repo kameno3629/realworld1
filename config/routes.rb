1ee4a9d3ab7c4daddd730fa4a11cda8c2db4f648
kameno3629 <blueocean777k@gmail.com>
2024-11-01 05:47:41 +0900

    [Fix]NewArticle_Function

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
       resource :follow, only: [:create, :destroy]
     end
 
-    resources :articles, param: :slug, except: [:edit, :new] do
+    resources :articles, param: :slug, except: [:new, :edit] do
       resource :favorite, only: [:create, :destroy]
       resources :comments, only: [:create, :index, :destroy]
       get :feed, on: :collection
@@ -23,4 +23,10 @@ Rails.application.routes.draw do
   
   # 追加: 単数形のルートが必要な場合
   get '/article/:slug', to: 'articles#show', as: :article_show
+
+  # newアクションのルート
+  get '/articles/new', to: 'articles#new', as: :new_article
+
+  # editアクションのルート
+  get '/articles/:slug/edit', to: 'articles#edit', as: :edit_article
 end
